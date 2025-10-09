ff7e29e4dc3e36a30097b441d422c9fcebe501cc
zaenalrfn <ZAOfficial24@gmail.com>
2025-10-09 21:48:58 +0700

    feat: setup model migration

---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'price',
+        'image',
+    ];
+
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function transactionItems()
+    {
+        return $this->hasMany(TransactionItem::class);
+    }
+}
